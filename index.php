631b6d772098d49d67f11d13b19d5d817673e111
akandshuvo <akandshuvo@gmail.com>
2016-03-31 18:25:59 +0600

    contact

---
+++ index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>DigiconContact</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="css/master.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="vendor/mdl/material.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="vendor/animate.css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+    <!-- Simple header with fixed tabs. -->
+      <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header
+                  mdl-layout--fixed-tabs">
+        <header class="mdl-layout__header">
+          <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <span class="mdl-layout-title">Digicon Contact center</span>
+          </div>
+          <!-- Tabs -->
+          <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
+            <a href="#it" class="mdl-layout__tab is-active">IT</a>
+            <a href="#fixed-tab-2" class="mdl-layout__tab">Tab 2</a>
+            <a href="#fixed-tab-3" class="mdl-layout__tab">Tab 3</a>
+            <a href="#fixed-tab-4" class="mdl-layout__tab">Tab 4</a>
+            <a href="#fixed-tab-5" class="mdl-layout__tab">Tab 5</a>
+            <a href="#fixed-tab-6" class="mdl-layout__tab">Tab 6</a>
+            <a href="#fixed-tab-7" class="mdl-layout__tab">Tab 7</a>
+          </div>
+        </header>
+        <div class="mdl-layout__drawer">
+          <span class="mdl-layout-title">Digicon Contact center</span>
+        </div>
+        <main class="mdl-layout__content">
+<!------------------------------------------------------------------------------------------------------>
+          <section class="mdl-layout__tab-panel is-active" id="it">
+            <div class="page-content">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--1-col"></div>
+
+                <div class="mdl-cell mdl-cell--5-col">
+                  <div class="contact">
+                    <div class="pic">
+                      <img src="image/img1.gif" alt="" />
+                    </div>
+                    <div class="information">
+                        ID:
+                        NAME:
+                        EMAIL:
+                        MOBILE:
+                        DEPARTMENT:
+                    </div>
+                  </div>
+                </div>
+                <div class="mdl-cell mdl-cell--5-col">
+                  <div class="contact">
+                    <div class="pic">
+                      <img src="image/img1.gif" alt="" />
+                    </div>
+                    <div class="information">
+                      ID:
+                      NAME:
+                      EMAIL:
+                      MOBILE:
+                      DEPARTMENT:
+                    </div>
+                  </div>
+                </div>
+
+                <div class="mdl-cell mdl-cell--1-col"></div>
+              </div>
+            </div>
+          </section>
+<!------------------------------------------------------------------------------------------------------>
+          <section class="mdl-layout__tab-panel" id="fixed-tab-2">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+
+          <section class="mdl-layout__tab-panel" id="fixed-tab-3">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+
+          <section class="mdl-layout__tab-panel" id="fixed-tab-4">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+
+          <section class="mdl-layout__tab-panel" id="fixed-tab-5">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+
+          <section class="mdl-layout__tab-panel" id="fixed-tab-6">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+
+          <section class="mdl-layout__tab-panel" id="fixed-tab-7">
+            <div class="page-content"><!-- Your content goes here --></div>
+          </section>
+        </main>
+      </div>
+
+
+
+
+
+    <script src="vendor/jquery-1.12.1.js" charset="utf-8"></script>
+    <script src="vendor/mdl/material.js" charset="utf-8"></script>
+    <script src="vendor/bootstrap/js/bootstrap.js" charset="utf-8"></script>
+  </body>
+</html>
